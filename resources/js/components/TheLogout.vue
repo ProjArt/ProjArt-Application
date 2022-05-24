7890d0403f74b15bde00836bad75b5d25363568e
Shammas44 <sebastien.traber@hotmail.com>
2022-05-24 09:28:30 +0200

    stockage du token dans un cookie

--- resources/js/components/TheLogout.vue
+++ resources/js/components/TheLogout.vue
@@ -2,17 +2,11 @@
 import { ref, toRaw } from "vue"
 import useFetch from "../composables/useFetch";
 import { API } from "../stores/api"
-import { useGetCookie, useSetCookie } from "../composables/useCookie";
 const isSubmitted = ref(false)
 const formData = ref({})
 const isAuthenticated = ref(false);
 console.log(window.location.href)
 
-try {
-    console.log(JSON.parse(getCookie('preferences')))
-} catch (error) {
-
-}
 const submitHandler = async () => {
     isSubmitted.value = true
     console.log(toRaw(formData.value))
@@ -24,8 +18,7 @@ const submitHandler = async () => {
         console.log(response);
         if (response.success === true) {
             isAuthenticated.value = true;
-            localStorage.setItem('token', response.data.access_token);
-            useSetCookie({ token: response.data.access_token }, 1);
+            localStorage.setItem('token', response.access_token);
             /* window.location.href += "signup"; */
         } else {
             isAuthenticated.value = false;
@@ -35,17 +28,16 @@ const submitHandler = async () => {
 </script>
 <template>
     <div>
-        <FormKit type="form" v-model="formData" :form-class="isSubmitted ? 'hide' : 'show'" submit-label="Login"
+        <FormKit type="form" v-model="formData" :form-class="isSubmitted ? 'hide' : 'show'" submit-label="Enregistrer"
             @submit="submitHandler">
-            <h2>Connexion</h2>
+            <h2>Création de compte</h2>
             <FormKit type="text" name="username" placeholder="unsername" validation="required" label="UserName" />
             <FormKit type="password" name="password" placeholder="password" validation="required" label="Password" />
         </FormKit>
         <div>
-            <h2 v-if="isAuthenticated && isSubmitted">Connexion établie</h2>
-            <h2 v-else-if="!isAuthenticated && isSubmitted">Mot de passe ou username non valid</h2>
+            <h2 v-if="isAuthenticated && isSubmitted">Compte crée</h2>
+            <h2 v-else-if="!isAuthenticated && isSubmitted">Echec de la création de compte</h2>
         </div>
-        <a class="forgot-password" href="#">Forgot password ?</a>
     </div>
 </template>
 <style>
--- resources/js/router/HomeRoute.vue
+++ resources/js/router/HomeRoute.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TheLogin from '../components/TheLogin.vue';
+import TheLogout from '../components/TheLogout.vue';
 </script>
 <template>
     <the-login></the-login>
