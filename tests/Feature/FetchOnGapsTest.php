204a1605840a59ef12b8c8175c20acb8820fef5f
Vincent <vincent.tarrit29@gmail.com>
2022-05-24 10:17:54 +0200

    fix indentation test gaps

--- tests/Feature/FetchOnGapsTest.php
+++ tests/Feature/FetchOnGapsTest.php
@@ -17,7 +17,7 @@ class FetchOnGapsTest extends TestCase
      */
     public function test_cannot_fetch_on_gaps_if_not_connected()
     {
-        $response = $this->json('GET',route('api.fetch.gaps.events'));
+        $response = $this->json('GET', route('api.fetch.gaps.events'));
 
         $response->assertUnauthorized();
     }
@@ -32,7 +32,4 @@ class FetchOnGapsTest extends TestCase
 
         $response->assertUnauthorized();
     }
-
-    
-
 }
