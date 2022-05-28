6a54cc578b688c268f14934965e261576b13485b
Vincent <vincent.tarrit29@gmail.com>
2022-05-29 00:09:39 +0200

    telegram

--- app/Models/TelegramChat.php
+++ app/Models/TelegramChat.php
@@ -8,7 +8,7 @@ class TelegramChat extends BaseModel
 {
     protected $table = 'telegraph_chats';
 
-    public function gapsUsers()
+    public function users()
     {
         return $this->belongsToMany(User::class, 'telegraph_chat_user', 'telegraph_chat_id', 'user_id');
     }
