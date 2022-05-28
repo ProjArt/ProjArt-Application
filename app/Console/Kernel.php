584a994c216b26512031bb494f008541a85102f1
Vincent <vincent.tarrit29@gmail.com>
2022-05-28 23:36:10 +0200

    fix schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             GapsMenuService::fetchMenus();
-        })->dailyAt('23:35');
+        })->dailyAt('11:02');
     }
 
     /**
