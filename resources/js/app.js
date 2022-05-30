aa27685ef78fa30177eb4ad870e18df8bcc77071
Vincent <vincent.tarrit29@gmail.com>
2022-05-30 21:39:23 +0200

    begining of service worker pwa

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import App from "./components/App.vue";
 import { plugin, defaultConfig } from "@formkit/vue";
 import router from "./router/routes";
 
+navigator.serviceWorker.register('/sw.js');
 const app = createApp(App).use(router);
 app.use(plugin, defaultConfig);
+
 app.mount("#app");
--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -5,8 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="DC-Covers">
+    <meta name="msapplication-TileImage" content="link to the image in static folder">
+    <meta name="msapplication-TileColor" content="#000">
+
+    <title>{{ config('app.name') }}</title>
     <base href="{{ env('MIX_BASE_URL') }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }} " />
+    <link rel="apple-touch-icon" href="link to the smaller icon">
+
     <link rel="stylesheet" href="{{ config('app.url') }}{{ mix('css/app.css') }}">
 
     {{-- ONE SIGNAL --}}
