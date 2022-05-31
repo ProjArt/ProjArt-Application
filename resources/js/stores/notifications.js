3f728ed0b11c8b5a18b4a4c45745b6b5246f6db0
Vincent <vincent.tarrit29@gmail.com>
2022-05-31 10:18:01 +0200

    send notif with design

--- resources/js/stores/notifications.js
+++ resources/js/stores/notifications.js
@@ -1,4 +1,6 @@
 import { ref, computed } from 'vue';
+import { API } from "./api";
+import useFetch from "../composables/useFetch";
 
 
 const _notifications = ref([]);
@@ -10,3 +12,18 @@ export const notification = computed({
         console.log("notifications", _notifications.value);
     }
 });
+
+export async function sendNotification({ title, message, to }) {
+
+    const response = await useFetch({
+        url: API.sendNotification.path(),
+        method: API.sendNotification.method,
+        data: {
+            title,
+            message,
+            to,
+        }
+    });
+
+    console.log("sendNotification", response);
+}
