98b459e9194c056a940b79e0d5f9d7a078ff888b
Shammas44 <sebastien.traber@hotmail.com>
2022-06-04 20:09:35 +0200

    quick save

--- resources/js/components/TheSettings.vue
+++ resources/js/components/TheSettings.vue
@@ -3,21 +3,21 @@ import { user } from "../stores/auth";
 import TheThemeManager from "./TheThemeManager.vue"
 import useLogout from "../composables/useLogout.js"
 
-async function disconnectAndRedirect(){
-  await useLogout();
-  
+function disconnectAndRedirect() {
+  useLogout();
+
 }
 </script>
 
 <template>
   <div class="settings-group">SETTINGS</div>
-  <Suspense><the-theme-manager/></Suspense>
-  <br/>
+  <!--<Suspense><the-theme-manager/></Suspense>-->
+  <br />
   <div class="user">{{ user }}</div>
   <div class="disconnect">
     <form class="disconnectForm" method="post" @submit.prevent="disconnectAndRedirect" action="/register">
-    <input type="submit" value="Se déconnecter"/>
-  </form>
+      <input type="submit" value="Se déconnecter" />
+    </form>
   </div>
 </template>
 
