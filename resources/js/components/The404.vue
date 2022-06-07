71de8a4cb428ab61aa79a6d6b072e876c0dfcb0e
Timothée Dione <timothee.dione@gmail.com>
2022-06-07 17:14:50 +0200

    page 404 pour iphone ok

--- resources/js/components/The404.vue
+++ resources/js/components/The404.vue
@@ -5,96 +5,96 @@ console.log("notFound");
 <template>
     <div class="main-body">
         <router-view />
-        <div class="quatre-zero-quatre-wrapper">
-                <h2 class="erreur-quatre-zero-quatre">ERREUR</h2>
-                <h1 class="quatre-zero-quatre-chiffre">404</h1>
+        <div class="title-wrapper">
+        <h2 class="erreur-quatre-zero-quatre">ERREUR</h2>
+        <h1 class="quatre-zero-quatre-chiffre">404</h1>
         </div>
-            <div class="indisponible-text">
-                La page que vous cherchez est malheureusement indisponible
-            </div>
-            <div class="back-to-home-link">
-                <a class="anchor-back-to-home" href="/">Retour à l'accueil</a>
-            </div>
-            <div class="logo">
-            <img class="anchor-back-to-home" src="/storage/images/logo.png" width="27.4%" style="position:relative">
-            </div>
+        <div class="indisponible-text">
+            La page que vous cherchez est malheureusement indisponible
+        </div>
+        <div class="back-to-home-link">
+            <a class="anchor-back-to-home" href="/">Retour à l'accueil</a>
+        </div>
+        <div class="logo">
+            <img
+                class="anchor-back-to-home"
+                src="/storage/images/logo.png"
+                width="27.4%"
+                style="position: relative"
+            />
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600;800&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;800&display=swap");
 
-main{
-    width:100vw;
+
+@media (max-width:600px){
+main {
+    width: 100vw;
 }
- .main-body {
+.main-body {
     background-color: var(--primary-color);
     color: var(--background-color);
     font-family: Poppins;
     height: 100vh;
-    width:100vw;
-} 
-
-/* Group 43 */
-.quatre-zero-quatre-wrapper{
-position: relative;
-width: 94.86%;
-height: 32.74%;
-left: 3.86%;
-/*top: 7.49%;*/
+    width: 100vw;
 }
 
+.title-wrapper{
+    position:absolute;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    transform: rotate(-33.55deg);
+    left:20%;
+}
 
 /* ERREUR */
-.erreur-quatre-zero-quatre{
-position: relative;
-left: 3.86%;
-right: 39.13%;
-top: 23.56%;
-bottom: 65.92%;
+.erreur-quatre-zero-quatre {
+    position: relative;
+    left:10%;
+    right: 39.13%;
+    bottom: 65.92%;
+    top:3%;
 
-margin-bottom:0px;
-width:fit-content;
+    margin-bottom: 0px;
+    width: fit-content;
 
-font-family: 'Poppins';
-font-style: bold;
-font-weight: 700;
-font-size: 3.3em;
-line-height: 5rem;
-display: flex;
-align-items: flex-end;
-text-align: center;
+    font-family: "Poppins";
+    font-style: bold;
+    font-weight: 700;
+    font-size: 3.3em;
+    line-height: 5rem;
+    display: flex;
+    align-items: flex-end;
+    text-align: center;
 
-color: #FFFFFF;
-
-transform: rotate(-33.55deg);
+    color: #ffffff;
 }
 
 /* 404 */
-.quatre-zero-quatre-chiffre{
-position: relative;
-line-height: 5rem;
-left: 35.27%;
-right: 7.73%;
-top: 24.96%;
+.quatre-zero-quatre-chiffre {
+    position: relative;
+    line-height: 5rem;
+    left: 35.27%;
+    right: 7.73%;
+    top: -5%;
 
-margin-top:0px;
-width: fit-content;
-top:0;
+    margin-top: 0px;
+    width: fit-content;
 
+    font-family: "Poppins";
+    font-style: bold;
+    font-weight: 700;
+    font-size: 5.8em;
+    display: flex;
+    align-items: flex-end;
+    text-align: center;
 
-font-family: 'Poppins';
-font-style: bold;
-font-weight: 700;
-font-size: 5.8em;
-display: flex;
-align-items: flex-end;
-text-align: center;
-
-color: #FFFFFF;
-
-transform: rotate(-31.34deg);
+    color: #ffffff;
 }
 
 /* La page que vous recherchez est malheureusement indisponible */
@@ -104,11 +104,11 @@ transform: rotate(-31.34deg);
     right: 22.5%;
     bottom: 57.47%;
     margin: 0px;
-    width:55%;
-    top:0%;
+    width: 55%;
+    top: 35%;
 
     font-family: "Poppins";
-    font-style: 'semi-bold';
+    font-style: "semi-bold";
     font-weight: 600;
     font-size: 20px;
     line-height: 30px;
@@ -119,34 +119,33 @@ transform: rotate(-31.34deg);
     color: #ffffff;
 }
 
-
 .back-to-home-link {
-    color:var(--background-color);
-    position:relative;
-    top:8%;
-    display:flex;
-    align-items:center;
-    justify-content:center;
+    color: var(--background-color);
+    position: relative;
+    top: 46%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background-color: var(--accent-color);
     left: 30%;
-    right:30%;
+    right: 30%;
     width: 40%;
-    height:6.8%;
+    height: 6.8%;
     border-radius: 1em;
     color: var(--background-color);
-    text-decoration:none;
+    text-decoration: none;
 }
 
-
-.logo{
-    position:relative;
-    top:16%;
-    display:flex;
-    justify-content:center;
+.logo {
+    position: relative;
+    top: 58%;
+    display: flex;
+    justify-content: center;
 }
 
-.anchor-back-to-home{
+.anchor-back-to-home {
     text-decoration: none;
     color: white;
+}
 }
 </style>
