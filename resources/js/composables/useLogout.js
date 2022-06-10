6f6ecc5ebaeac413cc3c760701c098242f219e7c
Vincent <vincent.tarrit29@gmail.com>
2022-06-10 17:13:51 +0200

    clean all on logout

--- resources/js/composables/useLogout.js
+++ resources/js/composables/useLogout.js
@@ -4,6 +4,9 @@ import { user } from "../stores/auth";
 
 const useLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("theme");
+    localStorage.removeItem("calendars");
     user.value = null;
     router.push("/");
 };
